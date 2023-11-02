08d7df159a4090c713c0e593d60b24ff1ae496c4
Juanjoarlandis <arlanfxtrading@gmail.com>
2023-11-02 22:34:07 +0100

    mm

--- src/components/MenuDrop.jsx
+++ src/components/MenuDrop.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import '../styles/MenuDrop.css';  // Importa el archivo CSS
+import tumbado from "../images/tumbado.png"
 
 function MenuDrop({ products, onCategoryChange }) {
     const uniqueDrops = [...new Set(products.map(product => product.drop))];
 
     return (
         <div className="menu-drop-container">
+            <img src={tumbado} alt="Hombre tumbado" className="man-tumbado" />
             <Link className="menu-drop-link" to="/" onClick={() => onCategoryChange("all")}>Todas las categorías</Link>
             {uniqueDrops.map(drop => (
                 <div key={drop}>
