e73aff0a3abe1427c1f7ceae7108c793937e0489
Juanjoarlandis <contact@juanjoarlandis.com>
2023-11-24 19:58:42 +0100

    aaaa

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,6 +7,7 @@ import cartIcon from "../images/cart.png"
 import aiIcon from "../images/ia.png"
 import menuIcon from "../images/menu.png"
 import medidaIcon from "../images/medidas.png"
+import galleryIcon from "../images/gallery.png"
 
 const NavBar = (props) => {
     return (
@@ -22,8 +23,8 @@ const NavBar = (props) => {
                     <img src={"https://rockystorage.s3.us-east-1.amazonaws.com/icons/cart.webp"} alt="Logo 1" className="logo" />
                     {props.cart.length > 0 && <span className="cart-counter">{props.totalItems + " items"}</span>}
                 </Link>
-                <Link to="/medidas" className="logo-container">
-                    <img src={"https://rockystorage.s3.us-east-1.amazonaws.com/icons/medidas.webp"} alt="Logo 3" className="logo" />
+                <Link to="/galeria" className="logo-container">
+                    <img src={galleryIcon} alt="Galería" className="logo" />
                 </Link>
                 <Link to="/rockyIA" className="logo-container">
                     <img src={"https://rockystorage.s3.us-east-1.amazonaws.com/icons/ia.webp"} alt="Logo 4" className="logo" />
